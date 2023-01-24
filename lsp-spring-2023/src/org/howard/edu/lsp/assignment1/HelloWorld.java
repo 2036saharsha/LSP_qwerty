6bcc3ca211ed8f170f1baf40b8539067e811bdce
2036saharsha <2036saharsha@gmail.com>
2023-01-23 23:30:29 -0500

    Adding assignment1

---
+++ lsp-spring-2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+*  Name: Saharsha Tiwari
+*/
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
