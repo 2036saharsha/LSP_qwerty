011c9074a307b124c543eec785653af8b9d5e0ef
2036saharsha <2036saharsha@gmail.com>
2023-03-16 13:27:07 -0400

    assignment 5

---
+++ lsp-spring-2023/src/org/howard/edu/lsp/assignment5/IntegerSet.java
@@ -0,0 +1,194 @@
+package org.howard.edu.lsp.assignment5;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+
+/**
+ * 
+ * @author saharsha
+ * IntegerSet is a implementation set which a list of integers that are unordered and unique.
+ * IntegerSet provides methods such as add, remove, clear, union, intersection , difference, etc which replicates the set.
+ */
+
+
+public class IntegerSet {
+	/**
+	 * Here set uses ArrayList to implement set like features
+	 *
+	 */
+	private List<Integer> set = new ArrayList<Integer>();
+	
+	/**
+	 * This is the Default Constructor .
+	 */
+	public IntegerSet() {
+		
+	}
+	
+	/**
+	 * Empties the IntegerSet to clear all the contents of the set using the .clear() method of ArrayList
+	 */
+	public void clear() {
+		set.clear();
+	}
+	
+	/**
+	 * It gives the length of the IntegerSet
+	 * @return length of the IntegerSet, using .size() method of ArrayList 
+	 */
+	
+	public int length() {
+		return set.size();
+	} 
+	
+	
+	/**
+	 * This method takes in a IntergerSet and checks if it equal with the other and returns a boolean value for it.
+	 * @param b another IntegerSet class
+	 * @return true if the two sets are equal or false otherwise
+	 * Two sets are considered to be equal if the elements of the sets are equal in any order.
+	 */
+	
+	public boolean equals(IntegerSet b) {
+		set.sort(null);
+		b.getSet().sort(null);
+		if (set.equals(b.getSet())) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	/**
+	 * This method takes in a integer and checks if it is present the set and return a boolean for it.
+	 * @param value a integer value to check if it contains in the set.
+	 * @return true if the IntegerSet contains the integer value, else false by using ArrayList's .contain() function
+	 */
+	
+	public boolean contains(int value) {
+		return set.contains(value);
+	}
+	
+	/**
+	 * This method returns the largest element in the set.	
+	 * @return the largest element in the set.
+	 * @throws IntegerSetException if the set is empty.
+	 */
+	
+	// Returns the largest item in the set; Throws a IntegerSetException if the set is empty 
+	public int largest() throws Exception {
+		if (this.getSet().isEmpty()) {
+			throw new IntegerSetException("IntegerSetException -> Set is Empty");
+	
+		} 	
+			
+		return (Collections.max(this.getSet()));
+	
+		
+	}
+	
+	/**
+	 * This method returns the smallest element in the set.
+	 * @return the smallest element in the set.
+	 * @throws IntegerSetException if the set is empty.
+	 */
+	
+	public int smallest() throws Exception {
+		if (this.getSet().isEmpty()) {
+			throw new IntegerSetException("IntegerSetException -> Set is Empty");
+		} 
+		
+		return Collections.min(set);
+	}
+	
+	/**
+	 * Adds an item to the set if it is not already present there
+	 * @param i a integer to add in the set if it is not present.
+	 */
+	
+	public void add(int i) {
+		if (!set.contains(i)) {
+			set.add(i);
+		}
+	}
+	
+	/**
+	 * This method removes the item passed from the set.
+	 * @param item a integer to remove from the set.
+	 */
+	
+	public void remove(int item) {
+		if (set.contains(item)) {
+			set.remove(Integer.valueOf(item));
+		}
+	}
+	
+	/**
+	 * This method performs Union of two sets.
+	 * @param intSetb a IntegerSet to perform union with.
+	 * Adds the elements of intSetb to MySet if it does not already contains it
+	 */
+	public void union(IntegerSet intSetb) {
+		List<Integer> setb=intSetb.getSet();
+		for (int i = 0; i < setb.size();i++) {
+			if (!set.contains(setb.get(i))){
+				set.add(setb.get(i));
+			}
+			
+		}	
+		
+	}
+	
+	/**
+	 * This method performs the intersection of the set.
+	 * @param intSetb a IntergerSet to perform the intersection with.
+	 */
+	
+	public void intersect(IntegerSet intSetb) {
+		this.getSet().retainAll(intSetb.getSet());
+	}
+	
+	/**
+	 * This method removes elements of second set which is in common with first set.
+	 * @param intSetb a IntegerSet whose elements are removed from first set.
+	 */
+	public void diff(IntegerSet intSetb) {
+		List<Integer> setb=intSetb.getSet();
+		for (int i=0; i<setb.size();i++) {
+			if (set.contains(setb.get(i))) {
+				set.remove(setb.get(i));
+			}
+		}
+	}
+	
+	/**
+	 * This methods checks if the set in empty and returns the boolean value accordingly.
+	 * @return a boolean value for set emptiness.
+	 */
+	
+	boolean isEmpty() {
+		return set.isEmpty();
+	}
+	
+	/**
+	 * This method converts the set to string.
+	 * @return string representation of the set.
+	 */
+	public String toString() {
+		return this.getSet().toString();
+	}
+	
+	/**
+	 * Getter to retrieve our ArrayList
+	 * @return the ArrayList set so that we can use it to reference other methods 
+	 */
+	
+	public List<Integer> getSet() {
+		return set;
+	}
+	
+	
+	
+}
---
+++ lsp-spring-2023/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -0,0 +1,250 @@
+package org.howard.edu.lsp.assignment5;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class IntegerSetTest {
+	@Test
+	@DisplayName("test add")
+	public void testAdd() {
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {6,2,9,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		assertEquals("[6, 2, 9, 4, 5]",setA.toString());
+		assertNotEquals("[]", setA.toString());
+		assertNotEquals("[1,2,3]", setA.toString());			
+	}
+	
+	@Test
+	@DisplayName("test clear")
+	public void testClear() {
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {6,2,9,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		setA.clear();
+		assertEquals("[]",setA.toString());
+		assertNotEquals("[6,2,9,4,5]", setA.toString());
+		assertNotEquals("", setA.toString());			
+	}
+	
+	@Test
+	@DisplayName("test remove")
+	public void testRemove() throws org.howard.edu.lsp.assignment5.IntegerSetException, IntegerSetException {
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {6,2,9,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		setA.remove(6);
+		setA.remove(2);
+		assertEquals("[9, 4, 5]",setA.toString());
+		assertNotEquals("[]", setA.toString());
+		assertNotEquals("[1,2,3]", setA.toString());			
+	}
+
+	@Test
+	@DisplayName("test contains")
+	public void testContains() {
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {6,2,9,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		assertEquals(true,setA.contains(6));
+		assertEquals(false,setA.contains(10));
+		assertNotEquals(true,setA.contains(50));
+	}
+	
+	@Test
+	@DisplayName("test isEmpty")
+	public void testEmpty() {
+		IntegerSet setA= new IntegerSet();
+		
+		assertEquals(true, setA.isEmpty());
+		
+		
+		int[] numbers = {6,2,9,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		
+		assertNotEquals(true,setA.isEmpty());
+		
+	}
+	
+	@Test
+	@DisplayName("test toString")
+	public void testToString() {
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {6,2,9,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		
+		assertEquals("[6, 2, 9, 4, 5]",setA.toString());
+		assertNotEquals("62945",setA.toString());
+	
+	}
+
+	@Test
+	@DisplayName("test union")
+	public void testUnion() {
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {6,2,10,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		IntegerSet setB= new IntegerSet();
+		int[] numbers1 = {6,2,5,7,9};
+		for (int i = 0; i < numbers1.length; i ++) {
+			setB.add(numbers1[i]);
+		}
+		setA.union(setB);
+		assertEquals("[6, 2, 10, 4, 5, 7, 9]",setA.toString());
+		assertNotEquals("[6, 2, 10, 4, 5, 6, 2, 5, 7, 9]",setA.toString());
+		
+	}
+	@Test
+	@DisplayName("test intersect")
+	public void testIntersect() {
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {1,2,3,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		IntegerSet setB= new IntegerSet();
+		int[] numbers1 = {1,3,5,7,9};
+		for (int i = 0; i < numbers1.length; i ++) {
+			setB.add(numbers1[i]);
+		}
+		setA.intersect(setB);
+		assertEquals("[1, 3, 5]",setA.toString());
+		assertNotEquals("[1,2,3,4,5]",setA.toString());
+	}
+	@Test
+	@DisplayName("test difference")
+	public void testDifference() {
+	 
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {1,2,3,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		IntegerSet setB= new IntegerSet();
+		int[] numbers1 = {1,3,5,7,9};
+		for (int i = 0; i < numbers1.length; i ++) {
+			setB.add(numbers1[i]);
+		}
+		setA.diff(setB);
+		assertEquals("[2, 4]",setA.toString());
+		assertNotEquals("[1, 3, 5]", setA.toString());
+	}
+
+	@Test
+	@DisplayName("test equals")
+	public void testEquals() {
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {1,2,3,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		IntegerSet setB= new IntegerSet();
+		int[] numbers1 = {1,2,3,4,5};
+		for (int i = 0; i < numbers1.length; i ++) {
+			setB.add(numbers1[i]);
+		}
+		assertEquals(setA.toString(),setB.toString());
+	
+	}
+
+	@Test
+	@DisplayName("test length")
+	public void testLength() {
+	
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {1,2,3,4,5};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		IntegerSet setB= new IntegerSet();
+		int[] numbers1 = {1,2,3,4,5};
+		for (int i = 0; i < numbers1.length; i ++) {
+			setB.add(numbers1[i]);
+		}
+		assertEquals(5,setA.length());
+		assertEquals(5,setB.length());
+		assertEquals(setA.length(),setB.length());
+	}
+
+	@Test
+	@DisplayName("test largest")
+	public void testLargest() throws Exception {
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {1,2,3,4,5,500,1000};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		IntegerSet setB= new IntegerSet();
+		int[] numbers1 = {1,2,3,4,500,700};
+		for (int i = 0; i < numbers1.length; i ++) {
+			setB.add(numbers1[i]);
+		}
+		assertEquals(1000,setA.largest());
+		assertEquals(700,setB.largest());
+	}
+
+	@Test
+	@DisplayName("TestLargest_throws_exception")
+	public void testLargest_THROWS_EXCEPTION() {
+		IntegerSet setA = new IntegerSet();
+		Exception exception = assertThrows(IntegerSetException.class, ()->{
+			setA.largest();
+		});
+		String expectedMessage = "IntegerSetException -> Set is Empty";
+		String actualMessage = exception.getMessage();
+		
+		assertTrue(actualMessage.equals(expectedMessage));
+		assertNotNull(exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("test smallest")
+	public void testSmallest() throws Exception {
+		IntegerSet setA= new IntegerSet();
+		int[] numbers = {1,2,3,4,5,500,1000};
+		for (int i = 0; i < numbers.length; i ++) {
+			setA.add(numbers[i]);
+		}
+		IntegerSet setB= new IntegerSet();
+		int[] numbers1 = {1,2,3,4,500,700};
+		for (int i = 0; i < numbers1.length; i ++) {
+			setB.add(numbers1[i]);
+		}
+		assertEquals(1,setA.smallest());
+		assertEquals(1,setB.smallest());
+	}
+
+	@Test
+	@DisplayName("TestSmallest_throws_exception")
+	public void testSmallest_THROWS_EXCEPTION() {
+		IntegerSet setA = new IntegerSet();
+		Exception exception = assertThrows(IntegerSetException.class, ()->{
+			setA.smallest();
+		});
+		String expectedMessage = "IntegerSetException -> Set is Empty";
+		String actualMessage = exception.getMessage();
+		
+		assertTrue(actualMessage.equals(expectedMessage));
+		assertNotNull(exception.getMessage());
+	}
+}
